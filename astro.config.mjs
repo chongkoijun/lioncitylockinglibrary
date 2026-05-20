d89ba7b9b0e046499a94df1abf85435e3c19eb16
Koi Jun Chong <koijun@192.168.1.252>
2026-05-20 13:40:41 +0800

    Fix sitemap domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lioncitylockinglibrary.com",
+  site: "https://www.lioncitylockinglibrary.com",
   adapter: cloudflare(),
   integrations: [sitemap()],
 });
